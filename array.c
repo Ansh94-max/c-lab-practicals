42fa9ab9c0abe9a4983a635eae4b06de23303ebf
Ansh94-max <ansh122021@gmail.com>
2026-02-16 12:11:18 +0530

    array,c

--- array.c
+++ array.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
 
-int main(){
-    int arr[20],n;
-    do{
-        printf("\n0. ");
+int main()
+{
+    int arr[20], n, choice;
+    do
+    {
+        printf("\n0.exit ");
         printf("\n1.create data");
+        printf("\n1.display data");
         printf("\n1.create data");
-        printf("\n1.create data");
-    }while();
-    printf("number of elements:");
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        printf("\nenter element no %d:",i+1);
-        scanf("%d",&arr[i]);
-    }
-    printf("\nArray elements are:");
-    for(int i=0;i<n;i++){
-        printf("%d ",arr[i]);
-    }
+        printf("enter your choice:");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+            ;
+        case 0:
+            break;
+        case 1:
+            printf("number of elements:");
+            scanf("%d", &n);
+            for (int i = 0; i < n; i++)
+            {
+                printf("\nenter element no %d:", i + 1);
+                scanf("%d", &arr[i]);
+            }
+            break;
+        case 2:
+            printf("\nArray elements are:");
+            for (int i = 0; i < n; i++)
+            {
+                printf("%d ", arr[i]);
+            }
+        }
+
+    } while (choice != 0);
+
     return 0;
 }
-
